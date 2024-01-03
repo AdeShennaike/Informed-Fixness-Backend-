afc767ff7cf58578aecfd0298fd4a76bd82a70d7
Ade <https://github.com/AdeShennaike>
2024-01-03 17:16:32 -0500

    got json data to populate

--- data/raw_exercise_data.js
+++ data/raw_exercise_data.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const fs = require('fs').promises; // Using fs promises API for cleaner async/await
+import { promises as fs } from 'fs'; // Using fs promises API for cleaner async/await
 
 // Replace 'API_ENDPOINT' with the actual endpoint of the API you're using
 const API_ENDPOINT = 'https://api.api-ninjas.com/v1/exercises';
@@ -9,7 +9,7 @@ async function fetchDataAndWriteToFile() {
     // Fetch data from the API
     const response = await axios.get(API_ENDPOINT,
         {headers: { 
-            'X-Api-Key': 'YY+/HA5kVPE7oPBhHTXJVg==f6wwv4BHCpXv8Ywk'}
+          'X-Api-Key': 'YY+/HA5kVPE7oPBhHTXJVg==f6wwv4BHCpXv8Ywk'}
         });
     const data = response.data;
     console.log(data)
@@ -17,7 +17,7 @@ async function fetchDataAndWriteToFile() {
     const formattedData = JSON.stringify(data, null, 2);
     
     // Save to exercise.json
-    await fs.writeFile('exercise.json', formattedData);
+    await fs.writeFile('./data/exercise.json', formattedData);
     console.log('Data saved to exercise.json');
   } catch (error) {
     console.error('Error fetching and writing data:', error);
