dfce462e5cad5876aa2e801230cc4a7425192b37
Ade Shennaike <shennaiek2386@hotmail.com>
2024-01-03 21:48:45 -0500

    working on seeds

---
+++ seed/00--seed-collections.js
@@ -0,0 +1,32 @@
+import exerciseRawData from '../data/exerciseRaw.json'
+import Exercise from '../../models/exercises.js'
+
+import mongoose from 'mongoose'
+
+// map function is a method that retuns a new arry oject from old arry data. we can use the map function to fenerate a new object for every object in the old array. you create a  new array, out of an old one, modifying the object as you go. in this case, our monarch object is the one we'll be applying the return logic to. that is a map function
+const exerciseData = exerciseRawData.map(exercise => {
+    return {
+        name: exercise.name,
+        muscle_group: exercise.muscle_group,
+        equipment: exercise.equipment
+    }
+    
+})
+
+
+//this is where you bring in the model reating a function to seed our database by removing everything and uploading a fresh set of data (using the map function from above).  Used to 'reset' a database without manual effort.  This is an anonomyous function, there's no need to name it.  All logic exists within the try block.  Mongoose will communicate with our database, whenever we use the capitol M Monarch term.
+const exerciseDatabase = async ()=>{
+    try {
+        await Exercise.deleteMany()
+        await Exercise.create(exerciseData)
+        console.log('Exercise Data Seeded')
+        mongoose.disconnect()
+    } catch (error) {
+        throw error
+    }
+}
+
+
+//this will execute our function
+// console.log(exerciseData)
+exerciseDatabase()
