5e519fa41d50b52ee86588562439098aba9c0f0e
Ade <https://github.com/AdeShennaike>
2024-01-04 22:01:04 -0500

    added every exercise from the api to my raw data then seeded my database

--- data/raw_exercise_data.js
+++ data/raw_exercise_data.js
@@ -1,20 +1,40 @@
 import axios from 'axios';
 import { promises as fs } from 'fs'; // Using fs promises API for cleaner async/await
 
-// Replace 'API_ENDPOINT' with the actual endpoint of the API you're using
-const API_ENDPOINT = 'https://api.api-ninjas.com/v1/exercises';
-
+const exercises = ['abdominals',
+  'abductors',
+  'adductors',
+  'biceps',
+  'calves',
+  'chest',
+  'forearms',
+  'glutes',
+  'hamstrings',
+  'lats',
+  'lower_back',
+  'middle_back',
+  'neck',
+  'quadriceps',
+  'traps',
+  'triceps']
+  
+  
+  // Replace 'API_ENDPOINT' with the actual endpoint of the API you're using
+// const API_ENDPOINT = `https://api.api-ninjas.com/v1/exercises;
+  
 async function fetchDataAndWriteToFile() {
-  try {
-    // Fetch data from the API
-    const response = await axios.get(API_ENDPOINT,
+  let allExercisesData = [];
+
+  for(let i = 0; i < exercises.length; i++){
+    try {
+      // Fetch data from the API
+    const response = await axios.get(`https://api.api-ninjas.com/v1/exercises?muscle=${exercises[i]}`,
         {headers: { 
           'X-Api-Key': 'YY+/HA5kVPE7oPBhHTXJVg==f6wwv4BHCpXv8Ywk'}
         });
-    const data = response.data;
-    console.log(data)
+      allExercisesData = allExercisesData.concat(response.data) 
     // Transform data here if necessary
-    const formattedData = JSON.stringify(data, null, 2);
+    const formattedData = JSON.stringify(allExercisesData, null, 2);
     
     // Save to exercise.json
     await fs.writeFile('./data/exerciseRaw.json', formattedData);
@@ -22,7 +42,7 @@ async function fetchDataAndWriteToFile() {
   } catch (error) {
     console.error('Error fetching and writing data:', error);
   }
+  }
 }
-
 // Run the function
 fetchDataAndWriteToFile();
